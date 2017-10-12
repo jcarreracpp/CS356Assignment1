0275f550b88be3a6bf2bd7da9a89177b161d5ef3
jcarreracpp <jcarrera@cpp.edu>
2017-10-12 16:06:48 -0700

    First commit.

---
+++ CS356Assignment1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs356assignment1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class CS356Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
